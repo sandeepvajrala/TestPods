531b49f1ec378a7ac7498a2ebfd827049816c065
sowjanyappl <sowjanya@peoplelinkvc.com>
2021-01-20 09:30:44 +0530

    init

---
+++ TestPods/TestPods.h
@@ -0,0 +1,18 @@
+//
+//  TestPods.h
+//  TestPods
+//
+//  Created by Peoplelink on 1/20/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestPods.
+FOUNDATION_EXPORT double TestPodsVersionNumber;
+
+//! Project version string for TestPods.
+FOUNDATION_EXPORT const unsigned char TestPodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestPods/PublicHeader.h>
+
+
---
+++ TestPods/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  TestPods
+//
+//  Created by Peoplelink on 1/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        let fantasticView = ColorView(frame: self.view.bounds)
+                
+        self.view.addSubview(fantasticView)
+        
+    }
+    
+
+}
